97b9369eed1b3c745dc16d07af7007d77bdf3544
Ahmed Mohiuddin Shah <mohiuddinlive@gmail.com>
2023-06-27 04:22:45 +0500

    removed unnecassary functions

--- lib/display_tag.dart
+++ lib/display_tag.dart
@@ -52,16 +52,4 @@ class _DisplayTagState extends State<DisplayTag> {
       }
     }
   }
-
-  Image imageFromBase64String(String base64String) {
-    return Image.memory(base64Decode(base64String));
-  }
-
-  Uint8List dataFromBase64String(String base64String) {
-    return base64Decode(base64String);
-  }
-
-  String base64String(Uint8List data) {
-    return base64Encode(data);
-  }
 }
