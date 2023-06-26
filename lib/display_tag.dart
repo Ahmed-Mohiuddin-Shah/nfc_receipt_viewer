577486a7fb0d9b42b8148e14c2e68f1d3b13aba3
Ahmed Mohiuddin Shah <mohiuddinlive@gmail.com>
2023-06-27 03:58:11 +0500

    Successfully Display record from nfc tag

--- lib/display_tag.dart
+++ lib/display_tag.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:nfc_manager/nfc_manager.dart';
+import 'dart:typed_data';
 
 class DisplayTag extends StatefulWidget {
   final NfcTag tag;
@@ -10,21 +13,38 @@ class DisplayTag extends StatefulWidget {
 }
 
 class _DisplayTagState extends State<DisplayTag> {
+  final ndefWidgets = <Widget>[];
+  Ndef? tech;
+
   @override
   Widget build(BuildContext context) {
+    parseResult();
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('NFC Receipt'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.save_rounded),
-      ),
-      body: Text(parseResult()),
-    );
+        appBar: AppBar(
+          title: const Text('NFC Receipt'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.save_rounded),
+        ),
+        body: Column(
+          children: ndefWidgets,
+        ));
   }
 
   String parseResult() {
+    tech = Ndef.from(widget.tag);
+    if (tech is Ndef && tech != null) {
+      final cachedMessage = tech!.cachedMessage;
+      for (var i in Iterable.generate(cachedMessage!.records.length)) {
+        String base64String =
+            const Utf8Decoder().convert(cachedMessage.records[i].payload);
+        ndefWidgets.add(
+          Text(base64String),
+        );
+      }
+    }
+
     List<String> stringList = widget.tag.data
         .toString()
         .toString()
