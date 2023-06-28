0fd98d0a6df6347ec88f8711a91308f22856ab07
Ahmed Mohiuddin Shah <mohiuddinlive@gmail.com>
2023-06-29 04:39:45 +0500

    Added methods t DataHandler

--- lib/data_handler.dart
+++ lib/data_handler.dart
@@ -1,3 +1,4 @@
+import 'package:nfc_receipt_viewer/receipt_class.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
@@ -11,6 +12,30 @@ class DatabaseHandler {
           "CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL,age INTEGER NOT NULL, country TEXT NOT NULL, email TEXT)",
         );
       },
+    );
+  }
+
+  Future<int> insertUser(List<Receipt> receipts) async {
+    int result = 0;
+    final Database db = await initializeDB();
+    for (var receipt in receipts) {
+      result = await db.insert('users', receipt.toMap());
+    }
+    return result;
+  }
+
+    Future<List<Receipt>> retrieveUsers() async {
+    final Database db = await initializeDB();
+    final List<Map<String, Object?>> queryResult = await db.query('users');
+    return queryResult.map((e) => Receipt.fromMap(e)).toList();
+  }
+  
+  Future<void> deleteUser(int id) async {
+    final db = await initializeDB();
+    await db.delete(
+      'users',
+      where: "id = ?",
+      whereArgs: [id],
     );
   }
 }
