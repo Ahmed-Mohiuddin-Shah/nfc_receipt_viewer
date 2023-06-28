e8520e82c6f496ab7e9deb5e4046ebbdbb7ecf97
Ahmed Mohiuddin Shah <mohiuddinlive@gmail.com>
2023-06-28 15:56:07 +0500

    added save receipt.dart

---
+++ lib/save_receipt.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SaveReceipt extends StatefulWidget {
+  const SaveReceipt({super.key});
+
+  @override
+  State<SaveReceipt> createState() => _SaveReceiptState();
+}
+
+class _SaveReceiptState extends State<SaveReceipt> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
